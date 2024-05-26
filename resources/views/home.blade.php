2e94e90a85a468df846be7a5af1be4d8338c6e49
AdityaWSN <adityawisnu@gmail.com>
2024-05-26 17:55:19 +0700

    Contoh pull

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,7 +15,7 @@
                     </div>
                     <div class="d-grid gap-3 col-6 mx-auto mt-3">
                         <button class="btn btn-outline-primary btn-lg" type="button">Pesan Disini</button>
-                        <button class="btn btn-outline-primary btn-lg" type="button">Lihat Antrian</button>
+                        <button class="btn btn-outline-primary btn-lg" type="button">Lihat Antrian !!!</button>
                         <br>
                     </div>
                 </div>
@@ -52,10 +52,10 @@
                     <div class="mt-2 display-3" style="font-family: 'Libre Franklin', sans-serif">
                     </div>
                     <div class="mt-4">
-                        <h6>Laundryen merupakan jasa laundry kiloan Lohbener & jasa cuci karpet kantor yang memiliki workshop di Jl.Lohbener Lama - Indramayu. 
+                        <h6>Laundryen merupakan jasa laundry kiloan Lohbener & jasa cuci karpet kantor yang memiliki workshop di Jl.Lohbener Lama - Indramayu.
                             <br>
                             <br>
-                            Kami menyediakan berbagai layanan laundry untuk meringankan rutinitas kegiatan mencuci, setrika pakaian setiap harinya. Untuk anda yang sibuk tidak sempat datang untuk mengantarkan cucian kotor ke workshop kami, tidak perlu khawatir kami memberikan layanan antar-jemput untuk laundry kiloan, cuci sepatu dll. 
+                            Kami menyediakan berbagai layanan laundry untuk meringankan rutinitas kegiatan mencuci, setrika pakaian setiap harinya. Untuk anda yang sibuk tidak sempat datang untuk mengantarkan cucian kotor ke workshop kami, tidak perlu khawatir kami memberikan layanan antar-jemput untuk laundry kiloan, cuci sepatu dll.
                             <br>
                             <br>
                             Anda bisa langsung pesan sekarang atau hubungi kontak kami, lalu tim kami akan melakukan pick up kerumah anda, ketika laundry selesai kami akan mengantarkan kembali kerumah anda.</h6>
