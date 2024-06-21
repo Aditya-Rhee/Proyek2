610dfffb60f268ae789b3161aae03772de23eba7
Rama Dwi Gusparaa <Ramaguspara16@gmail.com>
2024-06-21 07:41:01 +0700

    rama nambahin daftar paket

--- resources/views/pesan.blade.php
+++ resources/views/pesan.blade.php
@@ -1,32 +1,76 @@
 @extends('index')
 
 @section('content')
-<div class="bg-blue">
-    <div class="container mt-1">
-        {{-- baris kesatu --}}
-        <div class="row mt-1">
-            <div class="mt-5 col-4 d-flex flex-column align-items-center">
-                <div class="box montserrat"><h4>PAKET SETRIKA</h4>
-                <div><h6 class="text-merah">SETRIKA DENGAN <br> PELICIN PAKAIAN</h6></div> <br>
-                <h6 class="text-biru mb-1">RP.4.000/KG</h6>
-                <button class="btn btn-primary">Pesan Sekarang</button>
+<head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div class="box custom-box d-flex justify-content-between align-items-center">
+                <span class="text-left">
+                    <h4 class="text-bold">PAKET <br> SETRIKA</h4>
+                    <h6 class="text-merah">SETRIKA DENGAN <br> PELICIN PAKAIAN</h6>
+                    <h6 class="text-biru">Rp 4.000/KG</h6>
+                    <button class="btn btn-primary" style="width: 130px" >Pesan</button> 
+                </span>
+                <div style="max-width: 120px; height: 120px;">
+                    <img src="{{ asset('images/box1.png') }}" class="img-fluid">
                 </div>
             </div>
-            <div class="mt-5 col-4 d-flex flex-column align-items-center">
-                <div class="box montserrat"><h4>Paket Cuci Kering</h4></div>
-            </div>
-            <div class="mt-5 col-4 d-flex flex-column align-items-center">
-                <div class="box montserrat"><h4>Paket Cuci & Setrika</h4></div>
+        </div>
+        <div class="col-md-4">
+            <div class="box custom-box d-flex justify-content-between align-items-center">
+                <span class="text-left">
+                    <h4 class="text-bold">PAKET <br> CUCI KERING</h4>
+                    <h6 class="text-merah">CUCI DAN JEMUR <br> SAMPAI KERING</h6>
+                    <h6 class="text-biru">Rp 4.000/KG</h6>
+                    <button class="btn btn-primary" style="width: 130px" >Pesan</button> 
+                </span>
+                <div style="max-width: 120px; height: 120px;">
+                    <img src="{{ asset('images/box2.png') }}" class="img-fluid">
+                </div>
             </div>
         </div>
-
-        {{-- baris kedua --}}
-        <div class="row mt-5 mb-1">
-            <div class="col-6 d-flex flex-column align-items-center">
-                <div class="box"><h4>Lihat Antrian</h4></div>
+        <div class="col-md-4">
+            <div class="box custom-box d-flex justify-content-between align-items-center">
+                <span class="text-left">
+                    <h4 class="text-bold">PAKET <br> CUCI & SETRIKA</h4>
+                    <h6 class="text-merah">PAKET KOMPLIT <br> CUCI DAN SETRIKA</h6>
+                    <h6 class="text-biru">Rp 6.000/KG</h6>
+                    <button class="btn btn-primary" style="width: 130px" >Pesan</button> 
+                </span>
+                <div style="max-width: 120px; height: 120px;">
+                    <img src="{{ asset('images/box3.png') }}" class="img-fluid">
+                </div>
             </div>
-            <div class="col-6 d-flex flex-column align-items-center">
-                <div class="box"><h4>Paket Lainnya</h4></div>
+        </div>
+    </div>
+    <div class="row mt-4">
+        <div class="col-md-6">
+            <div class="box custom-box2 d-flex justify-content-between align-items-center">
+                <span class="text-left">
+                    <h4 class="text-bold">LIHAT ANTRIAN</h4>
+                    <h6 class="text-merah">KAMU BISA LIHAT UPDATE TERKINI <br> TERKAIT ANTRIANNYA, MEMANTAU <br> KAPAN SELESAI BAJU KAMU DICUCI</h6>
+                    <br>
+                    <a class="text-biru text-bold" href="{{ route('paketlainnya') }}"> DETAIL PAKET LAINNYA >> </a>
+                </span>
+                <div style="max-width: 120px; height: 120px;">
+                    <img src="{{ asset('images/box4.png') }}" class="img-fluid">
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="box custom-box3 d-flex justify-content-between align-items-center">
+                <span class="text-left">
+                    <h4 class="text-bold">PAKET LAINNYA</h4>
+                    <h6 class="text-putih">KAMI JUGA MENYEDIAKAN <br> JASA CUCI LAINNYA SEPERTI <br> SEPATU, KARPET DLL</h6>
+                    <br>
+                    <a class="text-putih text-bold" href="{{ route('paketlainnya') }}"> DETAIL PAKET LAINNYA >> </a>
+                </span>
+                <div style="max-width: 120px; height: 120px;">
+                    <img src="{{ asset('images/box5.png') }}" class="img-fluid">
+                </div>
             </div>
         </div>
     </div>
