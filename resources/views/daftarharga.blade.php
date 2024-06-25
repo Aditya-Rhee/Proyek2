a99ca1902dbd0e62fb9ed813a6c27127e7fa3a45
AdityaWSN <adityawisnu@gmail.com>
2024-06-25 14:56:48 +0700

    env

--- resources/views/daftarharga.blade.php
+++ resources/views/daftarharga.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <div class="container">
-        Daftar Harga
+        <div class="et_pb_text_inner fw-bold" style="font-size: 3vw; line-height: 1.2em; color: #000000; text-align: left; font-family: 'Lato', serif; text-transform: none; letter-spacing: 0px; font-style: normal; text-decoration: none; word-spacing: 0px; background-color: #ffffff;">
+            Harga Laundry Kiloan
+        </div>
     </div>
 @endsection
