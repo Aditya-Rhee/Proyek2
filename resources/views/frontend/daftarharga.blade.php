e99c55ea16293e283e838031252043a6e7fe50bc
Rama Dwi Gusparaa <Ramaguspara16@gmail.com>
2024-07-05 20:14:55 +0700

    layanan Daftar Harga

--- resources/views/frontend/daftarharga.blade.php
+++ resources/views/frontend/daftarharga.blade.php
@@ -2,8 +2,135 @@
 
 @section('content')
     <div class="container">
-        <div class="et_pb_text_inner fw-bold" style="font-size: 3vw; line-height: 1.2em; color: #000000; text-align: left; font-family: 'Lato', serif; text-transform: none; letter-spacing: 0px; font-style: normal; text-decoration: none; word-spacing: 0px; background-color: #ffffff;">
-            Harga Laundry Kiloan
+        <div class="mt-4 et_pb_text_inner fw-bold text-center" style="text-bold font-family: 'Libre Franklin', sans-serif">
+            <h2>DAFTAR HARGA PAKET LAUNDRYEN</h2>
+        </div>
+        <div class="mb-4" style="width:50%; flex-grow: 1; height: 1px; background-color: black; margin: 0 auto;"></div>
+
+        <div class="row mt-5">
+            <div class="col-md-6">
+                <h3 class="text-left">
+                    <img src="{{ asset('images/cuci_karpet.png') }}" style="width: 60px; height: 60px; margin-right: 10px;">
+                    Cuci Karpet
+                </h3>
+                <table class="table table-bordered" style="border: 1px solid black;">
+                    <thead style="border: 1px solid black;">
+                        <tr>
+                            <th style="border: 1px solid black;">Jenis Karpet</th>
+                            <th style="border: 1px solid black;">Harga per M²</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Karpet Wol</td>
+                            <td style="border: 1px solid black;">Rp 100,000</td>
+                        </tr>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Karpet Sintetis</td>
+                            <td style="border: 1px solid black;">Rp 150,000</td>
+                        </tr>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Karpet Nilon</td>
+                            <td style="border: 1px solid black;">Rp 150,000</td>
+                        </tr>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Karpet Polypropylene</td>
+                            <td style="border: 1px solid black;">Rp 150,000</td>
+                        </tr>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Karpet Sisal</td>
+                            <td style="border: 1px solid black;">Rp 150,000</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-6">
+                <h3 class="text-left">
+                    <img src="{{ asset('images/sepatu2.png') }}" style="width: 60px; height: 60px; margin-right: 10px;">
+                    Sepatu
+                </h3>
+                <table class="table table-bordered" style="border: 1px solid black;">
+                    <thead style="border: 1px solid black;">
+                        <tr>
+                            <th style="border: 1px solid black;">Jenis Sepatu</th>
+                            <th style="border: 1px solid black;">Harga</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Sepatu Canvas</td>
+                            <td style="border: 1px solid black;">Rp 50,000</td>
+                        </tr>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Sepatu Kulit Asli</td>
+                            <td style="border: 1px solid black;">Rp 75,000</td>
+                        </tr>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Sepatu Kulit Suede</td>
+                            <td style="border: 1px solid black;">Rp 75,000</td>
+                        </tr>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Sepatu Lak</td>
+                            <td style="border: 1px solid black;">Rp 75,000</td>
+                        </tr>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Sepatu Denim</td>
+                            <td style="border: 1px solid black;">Rp 75,000</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="row mt-5">
+            <div class="col-md-6">
+                <h3 class="text-left">
+                    <img src="{{ asset('images/furniture2.png') }}" style="width: 60px; height: 60px; margin-right: 10px;">
+                    Laundry Furniture
+                </h3>
+                <table class="table table-bordered" style="border: 1px solid black;">
+                    <thead style="border: 1px solid black;">
+                        <tr>
+                            <th style="border: 1px solid black;">Jenis Furniture</th>
+                            <th style="border: 1px solid black;">Harga</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Sofa</td>
+                            <td style="border: 1px solid black;">Rp 200,000</td>
+                        </tr>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Kursi</td>
+                            <td style="border: 1px solid black;">Rp 100,000</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="col-md-6">
+                <h3 class="text-left">
+                    <img src="{{ asset('images/satuan.png') }}" style="width: 60px; height: 60px; margin-right: 10px;">                   
+                     Laundry Satuan
+                </h3>
+                <table class="table table-bordered" style="border: 1px solid black;">
+                    <thead style="border: 1px solid black;">
+                        <tr>
+                            <th style="border: 1px solid black;">Item</th>
+                            <th style="border: 1px solid black;">Harga</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Kemeja</td>
+                            <td style="border: 1px solid black;">Rp 20,000</td>
+                        </tr>
+                        <tr style="border: 1px solid black;">
+                            <td style="border: 1px solid black;">Celana</td>
+                            <td style="border: 1px solid black;">Rp 25,000</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 @endsection
